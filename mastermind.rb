fcc0138c989b5d5071bbab6622f3cc7d930ebbf1
Andrew Gaytan <andrewutmail@gmail.com>
2023-07-13 10:55:53 -0500

    Added method to sequences which will create a user choice array of colors

--- mastermind.rb
+++ mastermind.rb
@@ -1,14 +1,25 @@
-module ComputerSequence
-  def random_sequence()
-    4.times do
-      @sequence.push(@colors.sample(1).join())
+module Sequences
+  def random_sequence() # Generates a random sequence
+    4.times do # sample method picks a random color and returns an array
+      @sequence.push(@colors.sample(1).join()) # join needed to make single array
     end
+  end
 
+  def choice_array(choice1, choice2, choice3, choice4) # creates a user array
+    choice_sequence = [@colors[choice1-1],@colors[choice2-1],@colors[choice3-1],@colors[choice4-1]]
+  end
+end
+
+module Checks
+  def color_match(guess, answer)
+  end
+
+  def sequence_match(guess, answer)
   end
 end
 
 class Mastermind
-  include ComputerSequence
+  include Sequences, Checks
 
   def initialize
     @sequence = []
@@ -20,7 +31,6 @@ class Mastermind
   end
 end
 
-computer = Mastermind.new
-computer.random_sequence()
-p computer.sequence
 
+guess = ["green", "yellow", "orange", "purple"]
+answer = ["red", "green", "orange", "blue"]
