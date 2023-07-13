9b765c403e228d7c572ca108e5058a0ac299f26e
Andrew Gaytan <andrewutmail@gmail.com>
2023-07-13 12:07:54 -0500

    Added color_match function to Checks module. Returns number of correct color guesses

--- mastermind.rb
+++ mastermind.rb
@@ -3,15 +3,26 @@ module Sequences
     4.times do # sample method picks a random color and returns an array
       @sequence.push(@colors.sample(1).join()) # join needed to make single array
     end
+    @sequence
   end
 
-  def choice_array(choice1, choice2, choice3, choice4) # creates a user array
+  def user_guess(choice1, choice2, choice3, choice4) # creates a user array
     choice_sequence = [@colors[choice1-1],@colors[choice2-1],@colors[choice3-1],@colors[choice4-1]]
   end
 end
 
 module Checks
   def color_match(guess, answer)
+    color_matches = [] # will store any matched colors
+    computer = answer # needed so as to not overwrite answer array
+
+    guess.each do |item| # iterate over guess
+      if computer.include?(item) # finds match in answer array
+        computer.delete_at(computer.index(item)) # delete from answer array
+        color_matches.push(item) # add item to color match
+      end
+    end
+    color_matches.length # returns number of matched colors
   end
 
   def sequence_match(guess, answer)
@@ -32,5 +43,9 @@ class Mastermind
 end
 
 
-guess = ["green", "yellow", "orange", "purple"]
-answer = ["red", "green", "orange", "blue"]
+# game = Mastermind.new
+# p comp_sequence = game.random_sequence()
+# p my_guess = game.user_guess(2,3,4,5)
+# p colors_correct = game.color_match(comp_sequence, my_guess)
+
+
